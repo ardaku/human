9e95ba3f099be8f8b5578e82efef2665db10461e
Jeron Aldaron Lau <aldaronlau@gmail.com>
2022-07-10 15:32:00 -0500

    Committing some old stuff, that's probably going to change.

--- src/key.rs
+++ src/key.rs
@@ -100,8 +100,8 @@ impl Debug for Mod {
 #[derive(Debug, Copy, Clone)]
 #[non_exhaustive]
 pub enum Key {
-    /// Also known as the ESCAPE key.
-    Back = 0x00u8,
+    /// The Escape Key
+    Esc = 0x00u8,
     /// Numeric 1 on either top row or numpad.
     One = 0x01,
     /// Numeric 2 on either top row or numpad.
@@ -127,97 +127,97 @@ pub enum Key {
     /// Equal Sign / Plus Key
     Equal = 0x0C,
     /// Backtick / Tilde Key
-    Backtick = 0x0D,
+    Backslash = 0x0D,
+    /// Backtick / Tilde Key
+    Backtick = 0x0E,
     /// Tab
-    Tab = 0x0E,
+    Tab = 0x0F,
     /// Q (may be named by a different glyph depending on language of user).
-    Q = 0x0F,
+    Q = 0x10,
     /// W (may be named by a different glyph depending on language of user).
-    W = 0x10,
+    W = 0x11,
     /// E (may be named by a different glyph depending on language of user).
-    E = 0x11,
+    E = 0x12,
     /// R (may be named by a different glyph depending on language of user).
-    R = 0x12,
+    R = 0x13,
     /// T (may be named by a different glyph depending on language of user).
-    T = 0x13,
+    T = 0x14,
     /// Y (may be named by a different glyph depending on language of user).
-    Y = 0x14,
+    Y = 0x15,
     /// U (may be named by a different glyph depending on language of user).
-    U = 0x15,
+    U = 0x16,
     /// I (may be named by a different glyph depending on language of user).
-    I = 0x16,
+    I = 0x17,
     /// U (may be named by a different glyph depending on language of user).
-    O = 0x17,
+    O = 0x18,
     /// I (may be named by a different glyph depending on language of user).
-    P = 0x18,
+    P = 0x19,
     /// [ (may be named by a different glyph depending on language of user).
-    BracketOpen = 0x19,
+    BracketOpen = 0x1A,
     /// ] (may be named by a different glyph depending on language of user).
-    BracketClose = 0x1A,
-    /// Backspace.
-    Backspace = 0x1B,
-    /// Env (Also known as: Win, Super, Cmd, Search) Key
-    Env = 0x1C,
+    BracketClose = 0x1B,
+    /// Backspace / Delete key.
+    Delete = 0x1C,
+    /// Key Caps or Caps Lock Key
+    Caps = 0x1D,
     /// A (may be named by a different glyph depending on language of user).
-    A = 0x1D,
+    A = 0x1E,
     /// S (may be named by a different glyph depending on language of user).
-    S = 0x1E,
+    S = 0x1F,
     /// D (may be named by a different glyph depending on language of user).
-    D = 0x1F,
+    D = 0x20,
     /// F (may be named by a different glyph depending on language of user).
-    F = 0x20,
+    F = 0x21,
     /// G (may be named by a different glyph depending on language of user).
-    G = 0x21,
+    G = 0x22,
     /// H (may be named by a different glyph depending on language of user).
-    H = 0x22,
+    H = 0x23,
     /// J (may be named by a different glyph depending on language of user).
-    J = 0x23,
+    J = 0x24,
     /// K (may be named by a different glyph depending on language of user).
-    K = 0x24,
+    K = 0x25,
     /// L (may be named by a different glyph depending on language of user).
-    L = 0x25,
+    L = 0x26,
     /// ; (may be named by a different glyph depending on language of user).
-    Semicolon = 0x26,
+    Semicolon = 0x27,
     /// ' (may be named by a different glyph depending on language of user).
-    Apostrophe = 0x27,
-    /// Enter (Also Return).
-    Enter = 0x28,
+    Apostrophe = 0x28,
+    /// Return / Enter Key
+    Enter = 0x29,
     /// Left Shift Key
-    LShift = 0x29,
+    LShift = 0x2A,
     /// Z (may be named by a different glyph depending on language of user).
-    Z = 0x2A,
+    Z = 0x2B,
     /// X (may be named by a different glyph depending on language of user).
-    X = 0x2B,
+    X = 0x2C,
     /// C (may be named by a different glyph depending on language of user).
-    C = 0x2C,
+    C = 0x2D,
     /// V (may be named by a different glyph depending on language of user).
-    V = 0x2D,
+    V = 0x2E,
     /// B (may be named by a different glyph depending on language of user).
-    B = 0x2E,
+    B = 0x2F,
     /// N (may be named by a different glyph depending on language of user).
-    N = 0x2F,
+    N = 0x30,
     /// M (may be named by a different glyph depending on language of user).
-    M = 0x30,
+    M = 0x31,
     /// , (may be named by a different glyph depending on language of user).
-    Comma = 0x31,
+    Comma = 0x32,
     /// . (may be named by a different glyph depending on language of user).
-    Period = 0x32,
+    Period = 0x33,
     /// / (may be named by a different glyph depending on language of user).
-    Slash = 0x33,
-    /// \ (may be named by a different glyph depending on language of user).
-    Backslash = 0x34,
-    /// Up Arrow
-    Up = 0x35,
+    Slash = 0x34,
     /// Right Shift Key
-    RShift = 0x36,
+    RShift = 0x35,
+    /// Up Arrow
+    Up = 0x36,
     /// Left control
     LCtrl = 0x37,
     /// Left alt
     LAlt = 0x38,
     /// Space (or Left Thumb Button)
     Space = 0x39,
-    /// Compose Key (Alt Gr, Right Thumb Button, NumLock, ScrLk Key)
-    Compose = 0x3A,
+    /// Thumb Key (Alt Gr or Right Thumb Button)
+    Thumb = 0x3A,
     /// Right Alt
     RAlt = 0x3B,
     /// Right Control
@@ -229,7 +229,8 @@ pub enum Key {
     /// Right Arrow Key
     Right = 0x3F,
     
-    // Back = 0x40u8,
+    /// The Power Key
+    Power = 0x40,
     /// F1 Key
     F1 = 0x41,
     /// F2 Key
@@ -254,64 +255,91 @@ pub enum Key {
     F11 = 0x4B,
     /// F12 Key
     F12 = 0x4C,
+    /// Print Screen / Screenshot Key
+    Screenshot = 0x4D,
     /// Insert Key
-    Insert = 0x4D,
-    // Tab = 0x4E,
-    // Q = 0x4F,
-    // W = 0x50,
-    // E = 0x51,
-    // R = 0x52,
-    // T = 0x53,
-    // Y = 0x54,
-    // U = 0x55,
-    // I = 0x56,
-    // O = 0x57,
-    // P = 0x58,
-    // BracketOpen = 0x19,
-    // BracketClose = 0x1A,
-    /// The delete key
-    Delete = 0x5B,
-    /// The Caps Lock Key
-    CapsLock = 0x5C,
-    // A = 0x1D,
-    // S = 0x1E,
-    // D = 0x1F,
-    // F = 0x20,
-    // G = 0x21,
-    // H = 0x22,
-    // J = 0x23,
-    // K = 0x24,
-    // L = 0x25,
-    // Semicolon = 0x26,
-    // Apostrophe = 0x27,
-    // NumpadEnter = 0x28,
-    // LShift = 0x29,
-    // Z = 0x2A,
-    // X = 0x2B,
-    // C = 0x2C,
-    // V = 0x2D,
-    // B = 0x2E,
-    // N = 0x2F,
-    // M = 0x30,
-    // Comma = 0x31,
-    // Period = 0x32,
-    // Slash = 0x33,
-    // Backslash = 0x34,
-    /// Page Up
-    PageUp = 0x75,
-    // RShift = 0x36,
-    // LCtrl = 0x37,
-    // LAlt = 0x38,
-    /// Pause Key
-    Pause = 0x79,
-    // Compose = 0x3A,
-    // RAlt = 0x3B,
+    Insert = 0x4E,
+    /// Next Media Track Key
+    Next = 0x4F,
+    /// Toggle Microphone
+    Microphone = 0x50,
+    /// Web Launcher (WWW Key)
+    Web = 0x51,
+    /// E-mail Launcher (Mail Key)
+    Email = 0x52,
+    // R = 0x53,
+    // T = 0x54,
+    // Y = 0x55,
+    // U = 0x56,
+    // I = 0x57,
+    // O = 0x58,
+    /// Audio Player Launcher
+    Player = 0x59,
+    // BracketOpen = 0x5A,
+    /// Calculator Launcher
+    Calculator = 0x5B,
+    /// The Del/Delete Key
+    Del = 0x5C,
+    /// Previous Media Track Key
+    Prev = 0x5D,
+    /// Airplane Mode
+    Airplane = 0x5E,
+    /// Scroll Lock
+    Scroll = 0x5F,
+    /// Display Toggle
+    Display = 0x60,
+    // F = 0x61,
+    // G = 0x62,
+    /// Korean Han Key
+    Han = 0x63,
+    /// Korean Hanja Key
+    Hanja = 0x64,
+    /// Kana Toggle Key
+    Kana = 0x65,
+    // L = 0x66,
+    /// Convert Key
+    Convert = 0x67,
+    /// Non-Convert Key
+    NonConvert = 0x68,
+    /// Numpad Enter Key
+    NumEnter = 0x69,
+    /// Mute Key
+    Mute = 0x6B,
+    /// Volume Down Key
+    Quieter = 0x6C,
+    /// Volume Up Key
+    Louder = 0x6D,
+    /// Brightness Down Key
+    Dimmer = 0x6E,
+    /// Brightness Up Key
+    Brighter = 0x6F,
+    /// Num Lock
+    Num = 0x70,
     /// Context Menu
-    Menu = 0x7C,
+    Menu = 0x71,
+    /// Comma
+    Break = 0x72,
+    /// Period
+    Clear = 0x73,
+    /// Pause
+    Pause = 0x74,
+    /// Page Up
+    PageUp = 0x76,
+    /// Multimedia Track Play Key
+    Play = 0x79,
+    /// Multimedia Track Stop Key
+    Stop = 0x7A,
     /// Home
     Home = 0x7D,
     /// Page Down
     PageDown = 0x7E,
     /// End
     End = 0x7F,
+
+    // RESERVED: LShift = 0x6A,
+    // RESERVED: RShift = 0x75,
+    // RESERVED: LCtrl = 0x77,
+    // RESERVED: LAlt = 0x78,
+    // RESERVED: RAlt = 0x7B,
+    // RESERVED: RCtrl = 0x7C
 }
